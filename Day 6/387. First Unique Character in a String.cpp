7c27bd09aef61b0ccd7f695868ee63b5e771679a
Gayatri Ganesh Joshi <90371721+GayatriJoshi@users.noreply.github.com>
2023-01-08 14:12:49 +0530

    Create 387. First Unique Character in a String.cpp

---
+++ Day 6/387. First Unique Character in a String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n=s.length();
+        vector<int> freq(26,0);
+        for(int i=0;s[i]!='\0';i++)
+        {
+            freq[s[i]-'a']+=1;
+        }
+        for(int i=0;s[i]!='\0';i++)
+        {
+            if(freq[s[i]-'a']==1) return i;
+        }
+        return -1;
+    }
+};
