e8490ba1ee9f14adab6f13cd966599ece6db974a
Gayatri Ganesh Joshi <90371721+GayatriJoshi@users.noreply.github.com>
2023-01-08 14:04:57 +0530

    Create 217. Contains Duplicate.cpp

---
+++ Day 1/217. Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      unordered_set<int> s;
+      for(auto a:nums)
+      {
+          s.insert(a);
+      }
+      if(nums.size()==s.size()) return false;
+      return true;
+    }
+};
